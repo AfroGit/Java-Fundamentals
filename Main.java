844158bbc455f44b057331079f6a580154e4660f
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-08-22 19:40:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+/*Below is a class declaration. The class body (the area between the braces) contains all the code that provides for the life cycle of the objects created from the class: constructors for initializing new objects, declarations for the fields that provide the state of the class and its objects, and methods to implement the behavior of the class and its objects. */ 
+
+class MyClass {
+    // field, 
+  //   constructor, and 
+    // method declarations
+}
